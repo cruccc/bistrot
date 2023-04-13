ba6c5a3735293513a30957bb147378db78d0aa8a
cruccc <l2002cruciani@gmail.com>
2023-04-13 20:02:35 +0200

    aggiunta riquadro menu

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
     </head>
 <body>
     <?php require "navbar.html";?>
+
     <img class="img-fluid copertina" src="images/copertina_crop.jpeg">
+
+    <div class="container-fluid">
+        <div class="row riga riga1">
+            <div class="riquadro">
+                <h2>SCOPRI IL MENU'</h2>
+                <p>Il vero antipasto marchigiano con taglieri di affettati e formaggi selezionati, le nostre fritture miste e una selezione di crostini. Le nostre pizze, salate e dolci, la varietà di hamburger, le nostre tagliate e per concludere i nostri dolci freschi e fatti in casa! Cosa aspetti? Fai la tua scelta e vieni ad assaggiare le nostre delizie!</p>
+                <button href="#"><b>MENU'<b></button>
+            </div>
+        </div>
+    </div>
+
     <?php require "footer.html";?>
 </body>
 </html>
