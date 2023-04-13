a1e31a5d25ce38283d332091e4d69f2258ad1432
cruccc <l2002cruciani@gmail.com>
2023-04-13 13:08:21 +0200

    aggiunta file di base 2

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+    <?php require "head.html";?>
+<body>
+    <?php require "navbar.html";?>
+    <p>Testo di prova</p><br>
+    <p>Testo di prova</p><br>
+    <p>Testo di prova</p><br>
+    <p>Testo di prova</p><br>
+    <p>Testo di prova</p><br>
+    <p>Testo di prova</p><br>
+    <?php require "footer.html";?>
+</body>
+</html>
