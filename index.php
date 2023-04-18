f61dbf1969f52608a5627833df4d35873b164b1e
cruccc <l2002cruciani@gmail.com>
2023-04-18 18:46:44 +0200

    modifiche spacing e file system aggiunta cartella "/images/birre" per contenere i loghi delle birre

--- index.php
+++ index.php
@@ -7,10 +7,11 @@
 <body>
     <?php require "navbar.html";?>
 
-    <img class="img-fluid copertina" src="images/copertina_crop.jpeg">
-
     <div class="container-fluid">
-        <div class="row riga riga1">
+        <div class="row riga1">
+            <img class="img-fluid copertina" src="images/copertina_crop.jpeg">
+        </div>
+        <div class="row riga riga2">
             <div class="riquadro">
                 <h2>SCOPRI IL MENU'</h2>
                 <p>Il vero antipasto marchigiano con taglieri di affettati e formaggi selezionati, le nostre fritture miste e una selezione di crostini. Le nostre pizze, salate e dolci, la varietà di hamburger, le nostre tagliate e per concludere i nostri dolci freschi e fatti in casa! Cosa aspetti? Fai la tua scelta e vieni ad assaggiare le nostre delizie!</p>
@@ -34,7 +35,7 @@
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <div class="carousel-riquadro">
-                                <img src="images/hellison_beer.png" alt="Birra1" class="carousel-immagine img-fluid">
+                                <img src="images/birre/hellison_beer.png" alt="Birra1" class="carousel-immagine img-fluid">
                                 <h3 style="padding-top:20px;">Birra fratm1</h3>
                                 <hr>
                                 <p>Birrificio : Styles<br>Gradazione alcolica: 4.8% Vol<br>Descrizione:<br>Helles è uno stile di birra chiara tedesca a bassa fermentazione (ovvero una lager) prodotta per lo più a Monaco, in baviera. Il colore è giallo pagliarino con schiuma bianca pannosa e persistente.</p>
@@ -42,7 +43,7 @@
                         </div>
                         <div class="carousel-item">
                             <div class="carousel-riquadro">
-                                <img src="images/Lilly-Smoke.png" alt="Birra2" class="carousel-immagine img-fluid">
+                                <img src="images/birre/Lilly-Smoke.png" alt="Birra2" class="carousel-immagine img-fluid">
                                 <h3 style="padding-top:20px;">Birra fratm1</h3>
                                 <hr>
                                 <p>Birrificio : Styles<br>Gradazione alcolica: 4.8% Vol<br>Descrizione:<br>Helles è uno stile di birra chiara tedesca a bassa fermentazione (ovvero una lager) prodotta per lo più a Monaco, in baviera. Il colore è giallo pagliarino con schiuma bianca pannosa e persistente.</p>
@@ -50,7 +51,7 @@
                         </div>
                         <div class="carousel-item">
                             <div class="carousel-riquadro">
-                                <img src="images/PoggieVolpi_cerchiobirre2.jpg" alt="Birra3" class="carousel-immagine img-fluid">
+                                <img src="images/birre/PoggieVolpi_cerchiobirre2.jpg" alt="Birra3" class="carousel-immagine img-fluid">
                                 <h3 style="padding-top:20px;">Birra fratm1</h3>
                                 <hr>
                                 <p>Birrificio : Styles<br>Gradazione alcolica: 4.8% Vol<br>Descrizione:<br>Helles è uno stile di birra chiara tedesca a bassa fermentazione (ovvero una lager) prodotta per lo più a Monaco, in baviera. Il colore è giallo pagliarino con schiuma bianca pannosa e persistente.</p>
@@ -70,15 +71,15 @@
             </div>
             <div class="col-md"></div>
         </div>
-        <div class="row riga riga1" id="section-2" >
+        <div class="row riga riga2" id="section-2" >
             <div class="riquadro">
                 <h2>VIENI A TROVARCI</h2>
                 <p>VIALE TRENTO 97, 63900, FERMO (FM)<br>MARCHE, ITALIA<br>
                 <a href="tel:+39 0734226124" style="color: #000000">TELEFONO: +39 0734226124</a><br>
-                <a href="tel:+39 328 2139611" style="color: #000000">CELLULARE: +39 328 2139611</a></p>
+                <a href="tel:+39 3282139611" style="color: #000000">CELLULARE: +39 328 2139611</a></p>
             </div>
         </div>
-        <div class="row riga riga1">
+        <div class="row riga1">
             <div class="google-maps">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11639.595293407669!2d13.7338154!3d43.1696479!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x133211de6dfb01e1%3A0xb8289bf6a854ef0a!2sBistrot83!5e0!3m2!1sit!2sit!4v1681548251873!5m2!1sit!2sit" 
                         width="100" height="100" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade">
