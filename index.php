1901de1a7de041ac06ec4cf3d4178a79489e423e
Nicola Capancioni <nicolacapancioni@gmail.com>
2023-05-04 11:54:46 +0200

    add photo galley

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
                     </a>
                 </div>
                 <div style="display: flex; justify-content: center; padding-top: 80px">
-                    <button class="mybtn" onclick="document.location='menu.php'"><b>SCOPRI TUTTE LE BIRRE</b></button>
+                    <button class="my-btn" onclick="document.location='menu.php'"><b>SCOPRI TUTTE LE BIRRE</b></button>
                 </div>
             </div>
             <div class="col-md"></div>
@@ -100,22 +100,13 @@
         </div>
 
         <!--Riquadro contatti-->
-        <div class="row riga riga1" id="section-2" >
-            <div class="riquadro">
-                <h2>VIENI A TROVARCI</h2>
-                <p>VIALE TRENTO 97, 63900, FERMO (FM)<br>MARCHE, ITALIA<br>
-                <a href="tel:+39 0734226124" style="color: #000000">TELEFONO: +39 0734226124</a><br>
-                <a href="tel:+39 3282139611" style="color: #000000">CELLULARE: +39 328 2139611</a></p>
-            </div>
+        <div class="row riga riga1">
+            <?php require "vieni-a-trovarci.html";?>
         </div>
 
         <!--Mappa-->
         <div class="row riga1">
-            <div class="google-maps">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11639.595293407669!2d13.7338154!3d43.1696479!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x133211de6dfb01e1%3A0xb8289bf6a854ef0a!2sBistrot83!5e0!3m2!1sit!2sit!4v1681548251873!5m2!1sit!2sit" 
-                        width="100" height="100" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-                </iframe>
-            </div>
+            <?php require "mappa.html";?>
         </div> 
     </div>
     <!--Fine contenuto pagina home-->
