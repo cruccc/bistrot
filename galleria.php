627f67957d06b60e3679a4b9ad3ccbe5658ddd44
Nicola Capancioni <nicolacapancioni@gmail.com>
2023-05-04 17:34:01 +0200

    fix galley

--- galleria.php
+++ galleria.php
@@ -6,165 +6,172 @@
 
 <body>
   <?php require "navbar.html";?>
-<!-- Container for the image gallery -->
-  <div class="carousel-container">
+  <div class= "container-fluid">
+    <div class="row" style="background-color: white; padding:15px 0px">
+      <div class="col-sm"></div>
+      <div class="col-md-7 col-sm-8">
 
-    <!-- Full-width images with number text -->
-    <div class="mySlides">
-      <div class="numbertext">1 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+        <!-- Container per la galleria-->
+        <div class="carousel-container">
 
-    <div class="mySlides">
-      <div class="numbertext">2 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <!-- Immagine superiore e numero corrente -->
+          <div class="mySlides">
+            <div class="numbertext">1 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">3 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">2 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">4 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">3 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">5 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">4 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">6 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">5 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">7 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">6 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">8 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">7 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">9 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">8 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">10 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">9 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">11 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">10 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">12 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">11 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">13 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">12 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">14 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">13 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">15 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">14 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">16 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">15 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">17 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">16 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <div class="mySlides">
-      <div class="numbertext">18 / 18</div>
-        <img src="images/copertina.jpeg" style="width:100%">
-    </div>
+          <div class="mySlides">
+            <div class="numbertext">17 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <!-- Next and previous buttons -->
-    <a class="prev" style="text-decoration: none" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" style="text-decoration: none" onclick="plusSlides(1)">&#10095;</a>
+          <div class="mySlides">
+            <div class="numbertext">18 / 18</div>
+            <img src="images/copertina.jpeg" style="width:100%">
+          </div>
 
-    <!-- Image text -->
-    <div class="caption-container">
-      <p id="caption"></p>
-    </div>
+          <hr style="opacity: 0; margin: 7.5px 0px">
 
-    <!-- Thumbnail images -->
-    <div class="row">
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(1)" alt="">
+          <!-- Bottoni avanti e dietro -->
+          <a class="prev" style="text-decoration: none; padding:8px" onclick="plusSlides(-1)">&#10094;</a>
+          <a class="next" style="text-decoration: none; padding:8px" onclick="plusSlides(1)">&#10095;</a>
+
+            <!-- Thumbnail images -->
+            <div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(1)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(2)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(3)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(4)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg"style="width:100%" onclick="currentSlide(5)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(6)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(7)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(8)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(9)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(10)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(11)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(12)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(13)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(14)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(15)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(16)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(17)" alt="">
+              </div>
+              <div class="column">
+                <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(18)" alt="">
+              </div>
+
+          </div>
+        </div>
       </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(2)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(3)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(4)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg"style="width:100%" onclick="currentSlide(5)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(6)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(7)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(8)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(9)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(10)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(11)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(12)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(13)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(14)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(15)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(16)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(17)" alt="">
-      </div>
-      <div class="column">
-        <img class="demo cursor" src="images/copertina.jpeg" style="width:100%" onclick="currentSlide(18)" alt="">
-      </div>
+      <div class="col-sm"></div>
     </div>
   </div>
 
